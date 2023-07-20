ff5d0b591f15d98b5ceec2bdf3e86282e4e9f9c7
mcolakovic <mc20190132@student.fon.bg.ac.rs>
2023-07-20 10:26:40 +0200

    RADI JEJ

---
+++ api.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Activity struct {
+	Activity      string  `json:"activity"`
+	Type          string  `json:"type"`
+	Participants  int     `json:"participants"`
+	Price         float64 `json:"price"`
+	Link          string  `json:"link"`
+	Key           string  `json:"key"`
+	Accessibility float64 `json:"accessibility"`
+}
+
+func InitializeHandlers(router *gin.Engine) {
+	router.GET("/name/:name", getActivity)
+}
+
+func getActivity(c *gin.Context) {
+	name := c.Param("name")
+	response, err := getName(name)
+	if err != nil {
+		fmt.Printf("Error making the request: %s\n", err)
+		return
+	}
+	c.JSON(http.StatusOK, response.Activity)
+}
+
+func getName(name string) (*Activity, error) {
+	url := "https://www.boredapi.com/api/activity"
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	var activity Activity
+	if err := json.NewDecoder(response.Body).Decode(&activity); err != nil {
+		return nil, err
+	}
+	activity.Activity = name + ", " + activity.Activity
+
+	return &activity, nil
+}
+
+func main() {
+
+	router := gin.Default()
+	InitializeHandlers(router)
+	router.Run(":8080")
+
+}
